56e21f320b6ffd98f79156c2d621b114edde5b96
carlosmendes09klmn-collab <carlosmendes09klmn@gmail.com>
2025-08-13 11:31:35 -0300

    Add files via upload

---
+++ MyPerfil2.py
@@ -0,0 +1,15 @@
+ # Inputação de dados em variáveis...
+print("Digite o seu nome:")
+MyName = str(input())
+print("Digite a sua altura:")
+MyHeight = float(input())
+print("Digite a sua idade:")
+MyAge = int(input())
+
+ # Exibição do conteúdo das variáveis...
+print("Meu nome é", MyName, ".")
+print("A minha altura é", MyHeight, "metros")
+print("Minha idade é", MyAge, "anos.")
+
+git config --global user.name "carlosmendes09klmn-collab"
+git config --global user.email carlosmendes09klmn@gmail.com
